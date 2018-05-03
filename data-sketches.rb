2d8dfe13239ae8579e4b37c2bf27f93123c1ff9a
Jon Malkin <jmalkin@users.noreply.github.com>
2018-05-03 10:53:56 -0700

    Update data-sketches.rb
    fix artifact version number

--- data-sketches.rb
+++ data-sketches.rb
@@ -1,7 +1,7 @@
 class DataSketches < Formula
   desc "Command line interface for DataSketches"
   homepage "https://datasketches.github.io"
-  url "https://search.maven.org/remotecontent?filepath=com/yahoo/datasketches/sketches-cmd/0.1.0/sketches-cmd-0.0.1-with-shaded-core.jar"
+  url "https://search.maven.org/remotecontent?filepath=com/yahoo/datasketches/sketches-cmd/0.1.0/sketches-cmd-0.1.0-with-shaded-core.jar"
   version "0.1.0"
   sha256 "58d9909aed665cb4660b2eb42a853c95ba6e1816284d366385b1368a7e3f1e0b"
 
